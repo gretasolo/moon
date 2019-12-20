d13a5ff29447515c8e3236fb9ca2d46f06b0cd15
Greta Solomon <gs2806@nyu.edu>
2019-12-20 02:58:50 -0600

    abcd

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 let mainContent = ["http://bit.ly/34iwwm0","http://bit.ly/34bWvvu"]
-let nainContent = ["http://bit.ly/34iwwm0","http://bit.ly/34bWvvu"]
+let nainContent = ["http://www.ancientpages.com/wp-content/uploads/2019/08/changemoongoddess11.jpg","https://1.api.artsmia.org/full/62539.jpg"]
 let painContent = ["https://i.ibb.co/pQWQbSk/Screen-Shot-2019-12-20-at-1-47-38-AM.png", "http://www.silkqin.com/01mywk/themes/nvlopr/xxjw.gif"]
 let qainContent = ["https://i.ibb.co/wSwjVcB/Screen-Shot-2019-12-20-at-2-02-55-AM.png", "https://i.ibb.co/7Ct9cFY/Screen-Shot-2019-12-20-at-2-03-07-AM.png"]
 
@@ -40,11 +40,11 @@ function fcoMode (e) {
 
 一
 <div id = "story1"><p>
-It is said that Chang E waas married to Yi. Their story transformed into folk legends and poetry regarding the moon. In the myths and legends, Chang E stole a special elixir from Yi and drank it becoming immortal. It is said that she is trapped, living in the moon above the Moon Palace. It was known later on Taoist mythology, Hou Yi became the Sun King. </p>
+It is said that Chang E waas married to Yi. Their story transformed into folk legends and poetry regarding the moon. In the myths and legends, Chang E stole a special elixir from Yi and drank it under threat of a threatening male figure, then becoming immortal. It is said that she is trapped, living in the moon above the Moon Palace. It was known later on Taoist mythology, Hou Yi became the Sun King. </p>
 </div>
 二
 
-<div id = "story1"><p>
+<div id = "story2"><p>
   Chinese feminists have by now carefully re-read and rewritten the story of goddess Chang E. In their words, she was not confined to living solitarily on the moon because she stole the pill of immortality from her husband Hou Yi. Rather, she chose to live on the moon because it was nearest the earth, and she was forced by circumstances to swallow the elixir to free herself from the threat of having to belong to a man craving power and possession who asserted he had killed her husband. For as long as the light of the moon is merely spoken of as having its birth in the sun, decreasing in proportion to its distance from the solar ray, and being accordingly light or dark as the sun comes and goes, women will reject woman.</p>
 
   </div>
@@ -54,8 +54,8 @@ It is said that Chang E waas married to Yi. Their story transformed into folk le
 
       <div className= 'clickclack'>
       <p id= "piccos">
-      <img id="iconbutton"  src={(activate === "text1") ? mainContent[0] : mainContent[1]}  height="215" width="320" alt="valueeee"></img>
-      <img id="firstpic" src={(activate==="text2") ? mainContent[1] : mainContent[0]} height="300" width="200" alt="value"></img>
+      <img id="iconbutton"  src={(activate === "text1") ? mainContent[1] : mainContent[1]}  height="215" width="320" alt="valueeee"></img>
+      <img id="firstpic" src={(activate==="text2") ? mainContent[0] : mainContent[0]} height="300" width="200" alt="value"></img>
       </p>
       </div>
     
@@ -64,7 +64,7 @@ It is said that Chang E waas married to Yi. Their story transformed into folk le
         <button id = "text1" onClick={ecoMode}> <strong><p> No! </p></strong></button>
         <button id = "text2" onClick={ecoMode}> <strong><p> Yes! </p></strong></button>
         </div>
-        <p className ="displayco2">{(activate==="text1") ? "This version of Chang'E was written in the Ming Dynasty but a Chinese painter." : ""} {(activate==="text2") ? "This version of Chang'E was written by a number of Chinese feminist writers in the" : ""}</p>
+        <p className ="displayco2">{(activate==="text1") ? "As seen in this newer painting of Chang'E, she is floating, but it is her decision, to take the elixir and float up and live around the moon. She chose to live their because of lack of freedom on earth and she is not trapped inside." : ""} {(activate==="text2") ? "Yes. The older version as seen through this older painting demonstrates that she did take the elixier to become immortal, but it wasn't her choice, she wasn't forced via threat, and is trapped there 'til this day." : ""}</p>
         
       </div>
       </div>
@@ -75,7 +75,7 @@ It is said that Chang E waas married to Yi. Their story transformed into folk le
 
 <div className= 'dlickdlack'>
 <p id= "piccolo">
-<img id="jconbutton"  src={(activate === "text3") ? nainContent[0] : nainContent[1]}  height="215" width="320" alt="valueeee"></img>
+<img id="jconbutton"  src={(activate === "text3") ? nainContent[0] : nainContent[1]}  height="300" width="200" alt="valueeee"></img>
 <img id="girstpic" src={(activate==="text4") ? nainContent[1] : nainContent[0]} height="300" width="200" alt="value"></img>
 </p>
 </div>
@@ -88,7 +88,7 @@ It is said that Chang E waas married to Yi. Their story transformed into folk le
   <button id = "text2" onClick={fcoMode}> <strong><p> Old 嫦娥! </p></strong></button>
   <button id = "text3" onClick={fcoMode}> <strong><p> New 嫦娥! </p></strong></button>
   </div>
-  <p className ="fisplayco2">{(activate==="text2") ? "This version of Chang'E was written in the Ming Dynasty but a Chinese painter." : ""} {(activate==="text4") ? "This version of Chang'E was written by a number of Chinese feminist writers in the" : ""}</p>
+  <p className ="fisplayco2">{(activate==="text2") ? "A lot of the original paintings capture the original story of 嫦娥. It is revealing of the view of women at the time of the myth, and the other writing, art, and sentiments at the time." : ""} {(activate==="text4") ? "New feminist takes on 嫦娥 allow us to reflect on the more confining view of women in the past, but use the historical myths to reshape how we define the future of women empowerment." : ""}</p>
   
   
  
