38197c6cd94cc5b44169e9423d94cd1158347fb2
Greta Solomon <gs2806@nyu.edu>
2019-12-19 23:34:08 -0600

    a

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 
 let mainContent = ["http://bit.ly/34iwwm0","http://bit.ly/34bWvvu"]
 let nainContent = ["http://bit.ly/34iwwm0","http://bit.ly/34bWvvu"]
+let painContent = ["http://bit.ly/2SdbCCy", "http://bit.ly/34bWvvu"]
+let qainContent = ["http://bit.ly/2SdbCCy", "http://bit.ly/34bWvvu"]
 
 function App() {
   return (
@@ -23,7 +25,9 @@ function Profile(props) {
 function fcoMode (e) {
     setActivate (e.target.id);
  }
-
+ function gcoMode (e) {
+  setActivate (e.target.id);
+}
 
   return (
 
@@ -34,18 +38,20 @@ function fcoMode (e) {
 
 <h1><strong>The Story</strong> </h1>
 
-<u>1: </u>
+一
+<div id = "story1"><p>
+It is said that Chang E waas married to Yi. Their story transformed into folk legends and poetry regarding the moon. In the myths and legends, Chang E stole a special elixir from Yi and drank it becoming immortal. It is said that she is trapped, living in the moon above the Moon Palace. It was known later on Taoist mythology, Hou Yi became the Sun King. </p>
+</div>
+二
 
-<p>It is said that Chang E and Yi created a monogamous precedent, in honor of their descendants, the interpretation of the Moon story, there are more than folk legends and poetry of its spread. [6] in the myths and legends, Chang E due to stealing Yi from West Queen premises obtained the elixir and drinks it becoming immortal, living in the moon above the Moon Palace . Later Taoism in its mythology, and the Chang-e Luna too Yinxing Jun merged into one, monthly Taoist yin essence, known as the moon Huang Hua Suyao after St. yuan to Yinyuan Jun, also known as moon lunar Wong Jun filial piety Ming Wang, a goddess..</p>
+<div id = "story1"><p>
+  Chinese feminists have by now carefully re-read and rewritten the story of goddess Chang E. In their words, she was not confined to living solitarily on the moon because she stole the pill of immortality from her husband Hou Yi. Rather, she chose to live on the moon because it was nearest the earth, and she was forced by circumstances to swallow the elixir to free herself from the threat of having to belong to a man craving power and possession who asserted he had killed her husband. For as long as the light of the moon is merely spoken of as having its birth in the sun, decreasing in proportion to its distance from the solar ray, and being accordingly light or dark as the sun comes and goes, women will reject woman.</p>
 
-<u>2:</u>
-
-<p>Chinese feminists have by now carefully re-read and rewritten the story of goddess Chang E. In their words, the latter was not confined to living solitarily on the moon because she stole the pill of immortality from her husband Hou Yi (who later became the Sun-King). Rather, she chose to live on the moon because it was nearest the earth, and she was forced by circumstances to swallow the elixir to free herself from the threat of having to belong to a man craving for power and possession who asserted he had killed her husband.4 As long as the light of the moon is merely spoken of as having its birth in the sun, decreasing in proportion to its dis- tance from the solar ray, and being accordingly light or dark as the sun comes and goes, women will reject Woman.</p>
-
-
-
+  </div>
 
 <h1><strong>Explore the Many Visions of Chang' E </strong> </h1>
+
+
       <div className= 'clickclack'>
       <p id= "piccos">
       <img id="iconbutton"  src={(activate === "text1") ? mainContent[0] : mainContent[1]}  height="215" width="320" alt="valueeee"></img>
@@ -69,20 +75,56 @@ function fcoMode (e) {
 
 <div className= 'dlickdlack'>
 <p id= "piccolo">
-<img id="jconbutton"  src={(activate === "text2") ? nainContent[0] : nainContent[1]}  height="215" width="320" alt="valueeee"></img>
-<img id="girstpic" src={(activate==="text3") ? nainContent[1] : nainContent[0]} height="300" width="200" alt="value"></img>
+<img id="jconbutton"  src={(activate === "text3") ? nainContent[0] : nainContent[1]}  height="215" width="320" alt="valueeee"></img>
+<img id="girstpic" src={(activate==="text4") ? nainContent[1] : nainContent[0]} height="300" width="200" alt="value"></img>
 </p>
 </div>
 
 
-<div className = "dlickers">
-  <div id= "whichversion"> <p id="mine"> Which version of the story of Chang'E do you want? </p>
+<div className = 'dlickers'>
+  <div id= "xhichversion"> 
+  
+  <p id="mine"> Which version of the story of Chang'E do you want? </p>
   <button id = "text2" onClick={fcoMode}> <strong><p> Old 嫦娥! </p></strong></button>
   <button id = "text3" onClick={fcoMode}> <strong><p> New 嫦娥! </p></strong></button>
   </div>
   <p className ="fisplayco2">{(activate==="text2") ? "This version of Chang'E was written in the Ming Dynasty but a Chinese painter." : ""} {(activate==="text4") ? "This version of Chang'E was written by a number of Chinese feminist writers in the" : ""}</p>
   
-</div></div></div>
+  
+ 
+</div></div>
+
+<div className= 'elickelack'>
+      <p id= "piccono">
+      <img id="kconbutton"  src={(activate === "text5") ? painContent[0] : painContent[1]}  height="215" width="320" alt="valueeee"></img>
+      <img id="hirstpic" src={(activate==="text6") ? painContent[1] : painContent[0]} height="300" width="200" alt="value"></img>
+      </p>
+      </div>
+    
+      <div className = "elickers">
+        <div id= "eanyou"> <p id="nine"> Did Chang'Eeee drink the elixir? </p>
+        <button id = "text5" onClick={ecoMode}> <strong><p> Old 嫦娥! </p></strong></button>
+        <button id = "text6" onClick={ecoMode}> <strong><p> New 嫦娥! </p></strong></button>
+        </div>
+        <p className ="gisplayco2">{(activate==="text5") ? "This version of Chang'E was written in the Ming Dynasty but a Chinese painter." : ""} {(activate==="text2") ? "This version of Chang'E was written by a number of Chinese feminist writers in the" : ""}</p>
+        
+      </div>
+
+  <div className= 'flickelack'>
+  <p id= "piccopo">
+  <img id="lconbutton"  src={(activate === "text7") ? qainContent[0] : qainContent[1]}  height="215" width="320" alt="valueeee"></img>
+  <img id="hirstpic" src={(activate==="text8") ? qainContent[1] : qainContent[0]} height="300" width="200" alt="value"></img>
+  </p>
+  </div>
+
+  <div className = "elickers">
+    <div id= "eanyou"> <p id="oine"> Did Chang'Eeee frink the elixir? </p>
+    <button id = "text7" onClick={ecoMode}> <strong><p> Old 嫦娥! </p></strong></button>
+    <button id = "text8" onClick={ecoMode}> <strong><p> New 嫦娥! </p></strong></button>
+    </div>
+    <p className ="gisplayco2">{(activate==="text8") ? "This version of Chang'E was written in the Ming Dynasty but a Chinese painter." : ""} {(activate==="text2") ? "This version of Chang'E was written by a number of Chinese feminist writers in the" : ""}</p>
+    
+  </div></div>
   )
 }
 
